7c43b169845d2387782b758a857f8b623467dd57
Maycon de Aquino Oliveira Sousa <mayconaquino@ciandt.com>
2025-01-20 15:25:36 -0300

    first commit

---
+++ src/main/java/api_login_user/ApiLoginUserApplication.java
@@ -0,0 +1,13 @@
+package api_login_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLoginUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLoginUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api_login_user/ApiLoginUserApplicationTests.java
@@ -0,0 +1,13 @@
+package api_login_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLoginUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
